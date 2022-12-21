31118e4421883ca22d9a5c0c8da82214f7c993ee
SyruSTR <dargonoed25@mail.ru>
2022-12-21 13:11:05 +0100

    fix playerMove

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -1,58 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
     private CharacterController _charController;
     [SerializeField] private float speed;
-    [SerializeField] private float gravit;
-    [SerializeField] private float jump_force;
-    // Start is called before the first frame update
+    [SerializeField] private float gravity;
+    [SerializeField] private float jumpForce;
+    private float _jumpSpeed = 0.0f;
 
-    void Start()
+    private void Awake()
     {
         _charController = GetComponent<CharacterController>();
     }
-    void Update()
+    private void Update()
     {
-
-        float jump_speed = 0.0f;
         float vert = Input.GetAxis("Vertical");
         float hor = Input.GetAxis("Horizontal");
-        speed = 1;
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (_charController.isGrounded)
         {
-            speed *= 2f;
+            _jumpSpeed = 0;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                _jumpSpeed = jumpForce;
+            }
         }
-        if (Input.GetKeyDown(KeyCode.Space) && _charController.isGrounded)
-        {
-            jump_speed = jump_force;
+        //add graviation
+        _jumpSpeed += gravity * Time.deltaTime;
+        
+        //create direction
+        
+        Vector3 velocity = new Vector3(hor, .0f, vert);
+        velocity *= speed;
+        velocity = transform.TransformVector(velocity);
+        velocity.y = _jumpSpeed;
 
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            if (!_charController.isGrounded)
-            {
-                jump_speed -= gravit * Time.deltaTime;
-            }
-            Vector3 distination = new Vector3(transform.forward.x, 0.0f, transform.forward.z);
-            distination *= speed * Time.deltaTime;
-            distination.y += jump_speed;
-            _charController.Move(distination);
-        }
-        else
-        {
-            if (!_charController.isGrounded)
-            {
-                jump_speed -= gravit * Time.deltaTime;
-            }
-            Vector3 distination = new Vector3(hor, 0.0f, vert);
-            distination *= speed * Time.deltaTime;
-            distination.y += jump_speed;
-            _charController.Move(distination);
-        }
-        
-        
+        _charController.Move(velocity * Time.deltaTime);
+
     }
 }
--- Assets/Script/Rotation.cs
+++ Assets/Script/Rotation.cs
@@ -6,10 +6,6 @@ public class Rotation : MonoBehaviour
 {
     public float sensitivityHor = 9.0f;
     public float sensitivityVert = 9.0f;
-    private float minimumVet = -30;
-    private float maximumVet = 40;
-    private float maximumHor = 45;
-    private float minimumHor = -45;
     private float _rotationX = 0;
     private float _rotationY = 0;
 
@@ -22,18 +18,9 @@ public class Rotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _rotationX = Input.GetAxis("Mouse Y") * sensitivityVert;
-        _rotationX = Mathf.Clamp(_rotationX, minimumVet, maximumVet);
-        float delta = Input.GetAxis("Mouse X") * sensitivityHor;
-        _rotationY = RestrictAngle(transform.localEulerAngles.y + delta, minimumHor, maximumHor);
-        transform.localEulerAngles = new Vector3(_rotationX, _rotationY, 0);
+        _rotationX = Input.GetAxis("Mouse X") * sensitivityVert;
+        Debug.Log(_rotationY);
+        transform.rotation *= Quaternion.Euler(.0f,_rotationX,.0f);
     }
-        public static float RestrictAngle(float angle, float angleMin, float angleMax)
-        {
-            if (angle > 180)
-                angle -= 360;
-            else if (angle < -180)
-                angle += 360;
-            return angle;
-        }
+
 }
