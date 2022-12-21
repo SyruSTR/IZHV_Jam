863002ac348cfc8e81cfad52760eafb68452f697
20dinara03 <94635894+20dinara03@users.noreply.github.com>
2022-12-21 20:34:27 +0100

    Update Move.cs

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -10,6 +10,7 @@ public class Move : MonoBehaviour
     [SerializeField] private float gravity;
     [SerializeField] private float jumpForce;
     private float _jumpSpeed = 0.0f;
+    public bool hold = false;
 
     private void Awake()
     {
@@ -17,9 +18,22 @@ public class Move : MonoBehaviour
     }
     private void Update()
     {
+        if (hold)
+        {
+            gravity = 0;
+        }
+        else
+        {
+            gravity = -4;
+        }
+        speed = 1.5f;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed *= 2;
+        }
         float vert = Input.GetAxis("Vertical");
         float hor = Input.GetAxis("Horizontal");
-        if (_charController.isGrounded)
+        if (_charController.isGrounded || hold)
         {
             _jumpSpeed = 0;
             if (Input.GetKey(KeyCode.Space))
@@ -39,5 +53,18 @@ public class Move : MonoBehaviour
 
         _charController.Move(velocity * Time.deltaTime);
 
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("slope"))
+        {
+            _jumpSpeed = 0;
+            hold = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        hold = false;
     }
 }
